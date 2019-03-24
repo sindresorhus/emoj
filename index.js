7bfee423c8dee412847643ecb28b9e82a2599226
Yanis Benson <yanis.benson@gmail.com>
2019-03-24 16:26:12 +0300

    solves #30

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 'use strict';
 const got = require('got');
+const emojilib = require('emojilib');
 
-module.exports = async input => {
+const getGetdangoEmojis = async input => {
 	const {results} = await got('http://emoji.getdango.com/api/emoji', {
 		searchParams: {
 			q: input
@@ -9,4 +10,46 @@ module.exports = async input => {
 	}).json();
 
 	return results.map(result => result.text);
+};
+
+const getEmojilibEmojis = input => {
+	const reSource = input.toLowerCase().split(/\s/g)
+		.map(v => v.replace(/\W/g, ''))
+		.filter(v => v.length > 0)
+		.map(v => (v.length >= 4) ? v : '^' + v + '$')
+		.join('|');
+
+	if (reSource.length === 0) {
+		return [];
+	}
+
+	const re = new RegExp(reSource);
+	const emojis = [];
+
+	for (const [name, data] of Object.entries(emojilib.lib)) {
+		let matches = re.test(name);
+		for (const keyword of data.keywords) {
+			matches = matches || re.test(keyword);
+		}
+
+		if (matches) {
+			emojis.push(data.char);
+		}
+	}
+
+	return emojis;
+};
+
+module.exports = async input => {
+	const set = new Set();
+
+	for (const emoji of getEmojilibEmojis(input)) {
+		set.add(emoji);
+	}
+
+	for (const emoji of await getGetdangoEmojis(input)) {
+		set.add(emoji);
+	}
+
+	return [...set];
 };
