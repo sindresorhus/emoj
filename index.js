cc97717b4b51c4a783483cc68738fd6aeb1a2b0a
Sindre Sorhus <sindresorhus@gmail.com>
2020-03-05 21:50:51 +0800

    Meta tweaks

--- index.js
+++ index.js
@@ -12,35 +12,36 @@ const getGetdangoEmojis = async input => {
 	return results.map(result => result.text);
 };
 
-// This value was picked experimentally, substring search returns a lot of noise for shorter search words
+// This value was picked experimentally.
+// Substring search returns a lot of noise for shorter search words.
 const MIN_WORD_LENGTH_FOR_SUBSTRING_SEARCH = 4;
 
 const getEmojilibEmojis = input => {
-	const reSource = input.toLowerCase().split(/\s/g)
+	const regexSource = input.toLowerCase().split(/\s/g)
 		.map(v => v.replace(/\W/g, ''))
 		.filter(v => v.length > 0)
 		.map(v => v.length < MIN_WORD_LENGTH_FOR_SUBSTRING_SEARCH ? `^${v}$` : v)
 		.join('|');
 
-	if (reSource.length === 0) {
+	if (regexSource.length === 0) {
 		return [];
 	}
 
-	const re = new RegExp(reSource);
-	const emojis = [];
+	const regex = new RegExp(regexSource);
+	const emoji = [];
 
 	for (const [name, data] of Object.entries(emojilib.lib)) {
-		let matches = re.test(name);
+		let matches = regex.test(name);
 		for (const keyword of data.keywords) {
-			matches = matches || re.test(keyword);
+			matches = matches || regex.test(keyword);
 		}
 
 		if (matches) {
-			emojis.push(data.char);
+			emoji.push(data.char);
 		}
 	}
 
-	return emojis;
+	return emoji;
 };
 
 module.exports = async input => {
